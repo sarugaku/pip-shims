f0e5fda90b6ce9444af01b5aa28292620fd76527
Dan Ryan <dan@danryan.co>
2019-06-16 16:07:15 -0400

    Fix source distribution lookup
    Signed-off-by: Dan Ryan <dan@danryan.co>

--- src/pip_shims/shims.py
+++ src/pip_shims/shims.py
@@ -199,7 +199,8 @@ class _shims(object):
                 "9999",
             ),
             "SourceDistribution": (
-                ("operations.prepare.IsSDist", "7.0.0", "19.1.1"),
+                ("req.req_set.IsSDist", "7.0.0", "9.0.3"),
+                ("operations.prepare.IsSDist", "10.0.0", "19.1.1"),
                 ("distributions.source.SourceDistribution", "19.1.2", "9999"),
             ),
             "WheelDistribution": (

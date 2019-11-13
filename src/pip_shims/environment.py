28228ed7236fc97fa391d64fecf25000bb3f9a7e
Dan Ryan <dan@danryan.co>
2019-11-13 02:44:04 -0500

    Full rewrite
    Signed-off-by: Dan Ryan <dan@danryan.co>

---
+++ src/pip_shims/environment.py
@@ -0,0 +1,28 @@
+# -*- coding=utf-8 -*-
+import importlib
+import os
+
+
+def get_base_import_path():
+    base_import_path = os.environ.get("PIP_SHIMS_BASE_MODULE", "pip")
+    return base_import_path
+
+
+BASE_IMPORT_PATH = get_base_import_path()
+
+
+def get_pip_version():
+    pip = importlib.import_module(BASE_IMPORT_PATH)
+    version = getattr(pip, "__version__", None)
+    return version
+
+
+def is_type_checking():
+    try:
+        from typing import TYPE_CHECKING
+    except ImportError:
+        return False
+    return TYPE_CHECKING
+
+
+MYPY_RUNNING = os.environ.get("MYPY_RUNNING", is_type_checking())
